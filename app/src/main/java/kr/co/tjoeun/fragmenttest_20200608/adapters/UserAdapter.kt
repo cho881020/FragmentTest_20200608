d9896dba0550e4b03584b88156ce0a26cf511025
cho881020 <kj_cho@idealidea.co.kr>
2020-06-08 21:24:08 +0900

    getView에서 실제 데이터 반영

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/adapters/UserAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/adapters/UserAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.fragmenttest_20200608.R
 import kr.co.tjoeun.fragmenttest_20200608.datas.User
 
@@ -29,6 +30,25 @@ class UserAdapter(
 //        null일 가능성이 없어진 tempRow를 !!를 붙여서 row에 대입.
         val row = tempRow!!
 
+//        실 데이터 반영 : XML의 뷰 + 데이터객체의 값
+
+//        XML의 뷰를 가져오는 코드
+        val userInfoTxt = row.findViewById<TextView>(R.id.userInfoTxt)
+
+//        뿌려줄 줄에 맞는 데이터 값
+        val data = mList[position]
+
+//        이름 / 나이 / 성별 구해서 조합
+        
+        val age = 2020 - data.birthYear + 1
+
+        var gender = "여성"
+        if (data.gender) {
+            gender="남성"
+        }
+
+        userInfoTxt.text = "${data.name}(${age}세, $gender)"
+        
 
         return row
     }
