3e20bf0ccb76db5e9c86fcd0bb455edb969e7edb
cho881020 <kj_cho@idealidea.co.kr>
2020-06-08 20:06:55 +0900

    만들어둔 어댑터클래스 / 뷰페이저 연결

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.fragmenttest_20200608
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.fragmenttest_20200608.adapters.MyAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var myAdapter : MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        myAdapter = MyAdapter(supportFragmentManager)
+        myViewPager.adapter = myAdapter
     }
 
 }
