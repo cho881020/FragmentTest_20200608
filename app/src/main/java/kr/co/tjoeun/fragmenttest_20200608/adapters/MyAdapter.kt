66f10a9075239b4f4b8ced3c63ff2d820a4a9b23
cho881020 <kj_cho@idealidea.co.kr>
2020-06-08 21:51:37 +0900

    페이지별 제목 - Adapter에서 설정  - getPageTitle 함수를 오버라이딩

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/adapters/MyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/adapters/MyAdapter.kt
@@ -7,6 +7,14 @@ import kr.co.tjoeun.fragmenttest_20200608.fragments.MyProfileFragment
 import kr.co.tjoeun.fragmenttest_20200608.fragments.StudentListFragment
 
 class MyAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "프로필 화면"
+            else -> "수강생 목록"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position) {
             0 -> MyProfileFragment()
