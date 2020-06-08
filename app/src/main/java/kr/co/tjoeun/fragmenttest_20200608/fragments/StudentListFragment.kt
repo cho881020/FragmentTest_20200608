c86e681e4ca97dcde3dfd9647c62dc12996cb8bd
cho881020 <kj_cho@idealidea.co.kr>
2020-06-08 21:15:15 +0900

    학생 목록 실제로 추가

--- app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/fragments/StudentListFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragmenttest_20200608/fragments/StudentListFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.tjoeun.fragmenttest_20200608.R
+import kr.co.tjoeun.fragmenttest_20200608.datas.User
 
 class StudentListFragment : BaseFragment() {
+
+    val studentList = ArrayList<User>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +31,16 @@ class StudentListFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        addStudents()
+    }
 
+    fun addStudents() {
+        studentList.add(User("조경진", 1988, true))
+        studentList.add(User("김미영", 2010, false))
+        studentList.add(User("김성우", 1982, true))
+        studentList.add(User("김형근", 1989, true))
+        studentList.add(User("이용희", 1992, true))
+        studentList.add(User("이지수", 1984, false))
     }
 
 }
